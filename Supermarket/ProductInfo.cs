d3b08ae2dd796bf54962e5e0027c129fbc0d06e9
HristoHackera123 <hrisi.belezirev@gmail.com>
2024-06-05 01:01:31 +0300

    Implemented homework task for supermarket

---
+++ Supermarket/ProductInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public class ProductInfo
+    {
+        private double price;
+        private int quantity;
+
+        public double Price { get { return price; } set { price = value; } }
+        public int Quantity { get { return quantity; } set { quantity = value; } }
+
+        public ProductInfo(double price, int quantity) 
+        { 
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+    }
+}
---
+++ Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Dictionary<string, ProductInfo> dict = new Dictionary<string, ProductInfo>();
+            string[] answer = Console.ReadLine().Split().ToArray();
+            while (answer[0] != "stocked")
+            {
+                string productName = answer[0];
+                double price = double.Parse(answer[1]);
+                int stock = int.Parse(answer[2]);
+                if (dict.ContainsKey(productName))
+                {
+                    dict[productName].Price = price;
+                    dict[productName].Quantity += stock;
+                }
+                else
+                {
+                    dict[productName] = new ProductInfo(price, stock);
+                }
+                answer = Console.ReadLine().Split().ToArray();
+            }
+            foreach(string item in dict.Keys)
+            {
+                int itemQuantity = dict[item].Quantity;
+                double itemPrice = dict[item].Price;
+                double itemTotalPrice = itemPrice * itemQuantity;
+                Console.WriteLine($"{item}: ${itemPrice:f2} * {itemQuantity} = ${itemTotalPrice:f2}");
+                total+= itemTotalPrice;
+            }
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Grand total: ${total:f2}");
+        }
+    }
+}
